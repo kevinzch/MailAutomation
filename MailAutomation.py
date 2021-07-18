7a58b3922e2afa8b6d3b8046499866be2f1417df
Kevin <a645749023@gmail.com>
2021-07-18 23:23:09 +0900

    -Make some settings customizable. -Change default bodyformat to richtext.

--- MailAutomation.py
+++ MailAutomation.py
@@ -1,58 +1,83 @@
-#Python 3.8.3
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import win32com.client
 from datetime import datetime, timedelta, time
+import csv
+import os
 
-START_TIME_STR = "06:00:00"
-END_TIME_STR   = "22:00:00"
+START_TIME_STR = '06:00:00'
+END_TIME_STR   = '22:00:00'
 
 FOLDER_CALENDAR = 9  # Reference: https://docs.microsoft.com/en-us/office/vba/api/outlook.oldefaultfolders
-FORMAT_HTML = 2      # Reference: https://docs.microsoft.com/en-us/office/vba/api/outlook.olbodyformat
+BODY_FORMAT = 3      # 1: plain, 2: HTML, 3: richtext Reference: https://docs.microsoft.com/en-us/office/vba/api/outlook.olbodyformat
 
-MAIL_TO = "to@outlook.com"
-MAIL_CC = "cc@outlook.com"
-MAIL_SUBJECT_TAG = "【〇〇】"
-MAIL_SUBJECT_NAME = "〇〇"
+MAIL_SUBJECT_TAG = '【〇〇〇〇連絡】'
 
-MAIL_BODY_GREETING = "〇〇〇〇〇\n"
-MAIL_BODY_BORDER = "------------------------------------------------------------------"
-MAIL_BODY_SIGNOFF = "\n〇〇〇〇〇"
+MAIL_BODY_GREETING = '〇〇〇〇〇\n'
+MAIL_BODY_BORDER = '------------------------------------------------------------------'
+MAIL_BODY_SIGNOFF = '\n〇〇〇〇〇'
+
+class Settings:
+    settingFilePath = os.path.join(os.path.dirname(__file__), 'settings.csv')
+
+    toList = []
+    ccList = []
+    selfName = ''
+    supervisorName = ''
 
 def getCalendarItems(start, end):
     outlookNamespace = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
-    calItems = outlookNamespace.GetDefaultFolder(FOLDER_CALENDAR).Items
-    calItems.IncludeRecurrences = True
-    calItems.Sort("[Start]")
+    tmpCalItems = outlookNamespace.GetDefaultFolder(FOLDER_CALENDAR).Items
+    tmpCalItems.IncludeRecurrences = True
+    tmpCalItems.Sort("[Start]")
 
-    restriction = "[Start] >= '" + start.strftime("%Y-%m-%d %H:%M") + "' And [End] <= '" + end.strftime("%Y-%m-%d %H:%M") + "'"
-    calItems = calItems.Restrict(restriction)
+    tmpRestriction = "[Start] >= '" + start.strftime("%Y-%m-%d %H:%M") + "' And [End] <= '" + end.strftime("%Y-%m-%d %H:%M") + "'"
+    tmpCalItems = tmpCalItems.Restrict(tmpRestriction)
 
-    return calItems
+    return tmpCalItems
 
 def sendSchedule(mBody, date):
     outlook = win32com.client.Dispatch("Outlook.Application")
-    newMail = outlook.CreateItem(0)
-    newMail.BodyFormat = FORMAT_HTML
-    newMail.To = MAIL_TO
-    newMail.CC = MAIL_CC
-    newMail.Subject = MAIL_SUBJECT_TAG + MAIL_SUBJECT_NAME + " " + date.strftime("%m/%d")
-    newMail.Body = mBody
-    newMail.Display()
+    tmpMail = outlook.CreateItem(0)
+    tmpMail.BodyFormat = BODY_FORMAT
+    tmpMail.To = ';'.join(Settings.toList)
+    tmpMail.CC = ';'.join(Settings.ccList)
+    tmpMail.Subject = MAIL_SUBJECT_TAG + str(Settings.selfName) + " " + date.strftime("%m/%d")
+    tmpMail.Body = mBody
+    tmpMail.Display()
 
 def makeMailBody(calItems):
-    bodyList = []
-    bodyList.append(MAIL_BODY_GREETING)
-    bodyList.append(MAIL_BODY_BORDER)
+    tmpBodyList = []
+    tmpBodyList.append(MAIL_BODY_GREETING)
+    tmpBodyList.append(MAIL_BODY_BORDER)
     for item in calItems:
         subjectStr = item.subject
         timeStr = "{0}～{1}".format(item.start.strftime("%H:%M"), item.end.strftime("%H:%M"))
-        bodyList.append(subjectStr + ' ' + timeStr)
+        tmpBodyList.append(subjectStr + ' ' + timeStr)
 
-    bodyList.append(MAIL_BODY_BORDER)
-    bodyList.append(MAIL_BODY_SIGNOFF)
+    tmpBodyList.append(MAIL_BODY_BORDER)
+    tmpBodyList.append(MAIL_BODY_SIGNOFF)
 
-    mBody = '\n'.join(bodyList)
+    mBody = '\n'.join(tmpBodyList)
     return mBody
+
+def getSettings(filePath):
+    with open(filePath, encoding='utf-8') as tmpFile:
+        tmpData = csv.DictReader(tmpFile, delimiter=',')
+        tmpDict = {}
+        for row in tmpData:
+            for key, value in row.items():
+                if value is not None and value != '':
+                    try:
+                        tmpDict[key].append(value)
+                    except KeyError:
+                        tmpDict[key] = [value]
+        
+        Settings.toList = list(tmpDict['To'])
+        Settings.ccList = list(tmpDict['CC'])
+        Settings.selfName = str(tmpDict['SelfName'])
+        Settings.supervisorName = str(tmpDict['SupervisorName'])
 
 workDate = datetime.today().date() + timedelta(1)
 startTime = time.fromisoformat(START_TIME_STR)
@@ -60,6 +85,8 @@ startDateTime = datetime.combine(workDate, startTime)
 endTime = time.fromisoformat(END_TIME_STR)
 endDateTime = datetime.combine(workDate, endTime)
 
+getSettings(Settings.settingFilePath)
+print(str(Settings.selfName))
 calenderItems = getCalendarItems(startDateTime, endDateTime)
 mailBody = makeMailBody(calenderItems)
 sendSchedule(mailBody, workDate)
